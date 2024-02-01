cb56a4a84865f9087dfe249ba128cbf22f0b5ab2
mgrout81@gmail.com <mgrout81@gmail.com>
2024-02-01 23:46:24 +0000

    Mouse brain fix committed oligodendrocyte spelling

---
+++ Mouse/MouseBrain.celltypeslist.R
@@ -0,0 +1,97 @@
+celltypes = list(
+    "Tissue" = list(
+        "Immune" = list(
+            "T cell",
+            "Myeloid" = list(
+                "Microglia",
+                "Perivascular macrophages"
+            )
+        ),
+        "Ependymal cells" = list(
+            "Choroid plexus epithelial cells",
+            "Hypendymal",
+            "Neuroblasts",
+            "Tanycytes"
+        ),
+        "Vascular" = list(
+            "Pericytes",
+            "Vascular endothelial cells",
+            "Vascular leptomeningeal cells",
+            "Vascular smooth muscle cells"
+        ),
+        "Glia" = list(
+            "Astrocytes" = list(
+                "Astrocytes thalamus hypothalamus",
+                "Astrocytes cortex hippocampus"
+            ),
+            "Radial glia" = list(
+                "Olfactory ensheathing cells",
+                "Radial glia",
+                "Astrocytes Bergmann glia"
+            ),
+            "Oligodendrocyte" = list(
+                "Committed oligodendrocytes",
+                "Myelin-forming oligodendrocytes",
+                "Mature oligodendrocytes",
+                "Newly-formed oligodendrocytes",
+                "Oligodendrocytes precursor cells"
+            )
+        ),
+        "Neuron" = list(
+            "Excitatory neuron" = list(
+                "Telencephalon excitatory neruon" = list(
+                    "Cortical excitatory neuron" = list(
+                        "Excitatory neurons amygdala",
+                        "Excitatory neurons layer 1 piriform",
+                        "Excitatory neurons layer 2/3",
+                        "Excitatory neurons layer 4",
+                        "Excitatory neurons layer 5",
+                        "Excitatory neurons layer 6",
+                        "Excitatory neurons layer 5/6",
+                        "Excitatory neurons telencephalon"
+                    ),
+                    "Hippocampal excitatory neuron" = list(
+                        "Excitatory neurons hippocampal CA1",
+                        "Excitatory neurons hippocampal CA2",
+                        "Excitatory neurons hippocampal CA3",
+                        "Granule neurons",
+                        "Cajal-Retzius cells"
+                    )
+                ),
+                "Non-telencephalon excitatory neuron" = list(
+                    "Excitatory neurons di/mesencephalon",
+                    "Hindbrain excitatory neurons"
+                )
+            ),
+            "Peptidergic neruons" = list(
+                "Cholinergic neurons habenula",
+                "Dopaminergic neurons",
+                "Serotonergic neurons"
+            ),
+            "Inhibitory neuron" = list(
+                "Hindbrain inhibitory neurons",
+                "Inhibitory interneurons",
+                "Inhibitory neurons amygdala",
+                "Inhibitory neurons habenula hypothalamus",
+                "Inhibitory neurons habenula thalamus",
+                "Inhibitory neurons reticular nucleus",
+                "Telencephalon inhibitory neurons",
+                "Olfactory bulb inhibitory neurons",
+                "Purkinje cells",
+                "Neurogliaform cells",
+                "Interneurons",
+                "Interneuron selective interneurons",
+                "CCK interneurons",
+                "D1 medium spiny neurons",
+                "D2 medium spiny neurons"
+            )
+        )
+    )  
+)
+
+
+
+
+
+
+
