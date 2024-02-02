3c17c9757ba508762107a8fffdb706f662cad5bf
mgrout81@gmail.com <mgrout81@gmail.com>
2024-02-02 00:06:14 +0000

    initial brain

---
+++ Human/Brain/Brain.celltypeslist.R
@@ -0,0 +1,31 @@
+celltypes = list(
+    "Other" = list(
+        "Endothelial"
+    ),
+    "Neuronal" = list(
+        "Inhibitory neuron" = list(
+            "Inhibitory neuron A",
+            "Inhibitory neuron B",
+            "Inhibitory neuron C"
+        ),
+        "Excitatory neuron" = list(
+            "L2/3 neuron",
+            "L4 neuron",
+            "L6 neuron"
+        ),
+        "Glial" = list(
+            "Oligodendrocyte-like" = list(
+                "Oligodendrocyte precursor cell",
+                "Oligodendrocyte"
+            ),
+            "Microglia" = list(
+                "Microglia A",
+                "Microglia B"
+            ),
+            "Astrocyte" = list(
+                "Astrocyte A",
+                "Astrocyte B"
+            )
+        )
+    )
+)
