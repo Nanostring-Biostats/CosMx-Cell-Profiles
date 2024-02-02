bc0af34e8ab59bc4a3a1339c87a672aa0d332fd8
mgrout81@gmail.com <mgrout81@gmail.com>
2024-02-02 16:47:16 +0000

    initial IO

---
+++ Human/IO/IO.celltypeslist.R
@@ -0,0 +1,32 @@
+celltypes = list(
+    "Immune" = list(
+        "Lymphoid" = list(
+            "NK cell",
+            "Plasmacytoid dendritic cell",
+            "B cell" = list(
+                "Plasma",
+                "Plasmablast"
+            ),
+            "T cell" = list(
+                "T cell CD8",
+                "T cell CD4" = list(
+                    "T cell regulatory"
+                )
+            )
+        ),
+        "Myeloid" = list(
+            "Myoblast" = list(
+                "Neutrophil",
+                "Monocyte" = list(
+                    "Macrophage"
+                ),
+                "Mast cell",
+                "Dendritic cell"
+            )
+        )
+    ),
+    "Other" = list(
+        "Fibroblast",
+        "Endothelial"
+    )
+)
